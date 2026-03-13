33d70fbfd28b2c99c5b111682e19a4699f92b200
USER <holymushroom1234@gmail.com>
2026-03-13 15:33:04 +0700

    доступна загрузка файлов игрового мира из XML-файлов

--- Engine/Models/ItemPercentage.cs
+++ Engine/Models/ItemPercentage.cs
@@ -1,19 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Engine.Models
 {
-    public class Trader : LivingEntity
+    public class ItemPercentage
     {
         public int ID { get; }
+        public int Percentage { get; }
 
-        public Trader(int id, string name) : base(name, 9999, 9999, 9999)
+        public ItemPercentage(int id, int percentage)
         {
             ID = id;
+            Percentage = percentage;
         }
     }
 }
